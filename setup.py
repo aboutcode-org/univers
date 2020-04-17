8c557ce7840f07a1685661485f4e55a947c14329
Philippe Ombredanne <pombredanne@nexb.com>
2020-04-17 15:46:42 +0200

    Bump version
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-version = '0.0.1'
+version = '0.9.0'
 
 #### Small hack to force using a plain version number if the option
 #### --plain-version is passed to setup.py
