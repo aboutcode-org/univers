729c2a73d919ff97d1fe21d808ac7b33c8877c20
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2021-03-16 15:34:27 +0530

    Add rough implementation for VersionRange and VersionSpecification class
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>

--- src/versions.py
+++ src/versions.py
@@ -20,6 +20,10 @@ from packaging import version
 from functools import total_ordering
 
 
+def remove_spaces(string):
+    return string.replace(" ", "")
+
+
 class BaseVersion:
     # each version value should be comparable e.g. implement functools.total_ordering
 
@@ -45,17 +49,18 @@ class PYPIVersion(BaseVersion):
         self.validate(version_string)
         self.value = version.Version(version_string)
 
-    def validate(self, version_string):
+    @staticmethod
+    def validate(version_string):
         match = version.Version._regex.search(version_string)
         if not match:
             raise InvalidVersion(f"Invalid version: '{version_string}'")
 
     def __eq__(self, other):
         # TBD: Should this verify the type of `other`
-        return self.value.__eq__(other)
+        return self.value.__eq__(other.value)
 
     def __lt__(self, other):
-        return self.value.__lt__(other)
+        return self.value.__lt__(other.value)
 
 
 class InvalidVersion(ValueError):
@@ -99,7 +104,7 @@ versions_classes_by_scheme = {
     "generic": GenericVersion,
     "semver": SemverVersion,
     "debian": DebianVersion,
-    "pypi": PYPIVersion
+    "pypi": PYPIVersion,
     # ....
 }
 
@@ -120,7 +125,56 @@ def parse_version(version):
 class VersionRange:
     # one of <> >= =< or != or =
     operator = ""
-    range = ""
+    value = ""
+
+    def __init__(self, version_range_string):
+        version_range_string = remove_spaces(version_range_string)
+        self.operator, self.value = self.split_operator_and_value(version_range_string)
+
+    @staticmethod
+    def split_operator_and_value(version_range_string):
+
+        if ">=" in version_range_string:
+            return version_range_string.split(">=")
+
+        if "<=" in version_range_string:
+            return version_range_string.split("<=")
+
+        if "!=" in version_range_string:
+            return version_range_string.split("!=")
+
+        if "<" in version_range_string:
+            return version_range_string.split("<")
+
+        if ">" in version_range_string:
+            return version_range_string.split(">")
+
+        if "=" in version_range_string:
+            return version_range_string.split("=")
+
+        return "=", version_range_string
+
+    def __contains__(self, version):
+        version_class = versions_classes_by_scheme[version.scheme]
+        version_object = version_class(self.value)
+
+        # TODO: this can  be made more concise by using `eval`. Research whether that is safe
+        if ">=" in version_range_string:
+            return version >= version_object
+
+        if "<=" in version_range_string:
+            return version <= version_object
+
+        if "!=" in version_range_string:
+            return version != version_object
+
+        if "<" in version_range_string:
+            return version < version_object
+
+        if ">" in version_range_string:
+            return version > version_object
+
+        return version == version_object
 
 
 class VersionSpecifier:
@@ -129,19 +183,46 @@ class VersionSpecifier:
     ranges = []
 
     @classmethod
-    def from_version_spec_string(cls, value):
+    def from_version_spec_string(cls, version_spec_string):
         """
         Return a VersionSpecifier built from a version spec string, prefixed by
         a scheme such as "semver:1.2.3,>=2.0.0"
         """
-        raise NotImplementedError
+        scheme, _, version_range_expressions = version_spec_string.partition(":")
+        if not scheme:
+            raise ValueError(f"{version_spec_string} is not prefixed by scheme")
+
+        if not version_range_expressions:
+            raise ValueError(f"{version_spec_string} contains no version range")
+
+        return from_scheme_version_spec_string(scheme, version_range_expressions)
 
     @classmethod
     def from_scheme_version_spec_string(cls, scheme, value):
         """
         Return a VersionSpecifier built from a scheme-specific version spec string and a scheme string.
         """
-        raise NotImplementedError
+        version_ranges = value.split(",")
+        ranges = []
+        for version_range in version_ranges:
+            operator, value = VersionRange.split_operator_and_value(version_range)
+            version_class = versions_classes_by_scheme[scheme]
+            version_class.validate(value)
+            ranges.append(VersionRange(version_range))
+
+        vs = VersionSpecifier()
+        vs.ranges = ranges
+        vs.scheme = scheme
+        return vs
+
+    def __str__(self):
+        """
+        Return this VersionSpecifier string using a canonical representation and our universal syntax.
+        """
+        # TODO: sort to make canonic
+        ranges = ",".join(self.ranges)
+
+        return f"{self.scheme}:{ranges}"
 
     def __contains__(self, version):
         """
@@ -152,13 +233,4 @@ class VersionSpecifier:
         if isinstance(version, str):
             version = parse_version(version)
 
-        # .... magic happens here
-        raise NotImplementedError
-
-    def __str__(self):
-        """
-        Return this VersionSpecifier string using a canonical representation and our universal syntax.
-        """
-        # TODO: sort to make canonic
-        ranges = ",".join(self.ranges)
-        return f"{self.scheme}:{ranges}"
+        return all([version in version_range for version_range in self.ranges])
