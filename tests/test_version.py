e1f58ce0724eb73bcf3c89784d3be9d6876ba2ed
Philippe Ombredanne <pombredanne@nexb.com>
2021-03-22 16:34:59 +0100

    Improve comparison of versions
    In particular versions with tilde.
    Based on https://github.com/memory/python-dpkg/issues/3 our code did not
    work. This changes the version parsing and compaison for a simpler way
    mostly based on modified @xolox deb_pkg_tools.
    
    
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/test_version.py
+++ tests/test_version.py
@@ -14,7 +14,9 @@ from __future__ import unicode_literals
 from unittest import TestCase
 
 from debut import version
-
+from debut.version import compare_strings
+from debut.version import compare_versions
+from debut.version import Version
 
 """
 Parse, compare and sort Debian package versions.
@@ -24,135 +26,146 @@ comparison code.
 """
 
 
+def get_non_digit_prefix(s):
+    val = version.get_non_digit_prefix(list(s))
+    return ''.join(val)
+
+
+def get_digit_prefix(s):
+    return version.get_digit_prefix(list(s))
+
+
 class DebianVersionTest(TestCase):
 
-    def test_get_epoch(self):
-        self.assertEqual(version.get_epoch('0'), (0, '0'))
-        self.assertEqual(version.get_epoch('0:0'), (0, '0'))
-        self.assertEqual(version.get_epoch('1:0'), (1, '0'))
-        self.assertRaises(Exception, version.get_epoch, '1a:0')
+    def test_Version_from_string_epoch(self):
+        assert Version.from_string('0').epoch == 0
+        assert Version.from_string('0:0').epoch == 0
+        assert Version.from_string('1:0').epoch == 1
 
-    def test_get_upstream(self):
-        self.assertEqual(version.get_upstream('00'), ('00', '0'))
-        self.assertEqual(version.get_upstream('foo'), ('foo', '0'))
-        self.assertEqual(version.get_upstream('foo-bar'), ('foo', 'bar'))
-        self.assertEqual(version.get_upstream('foo-bar-baz'), ('foo-bar', 'baz'))
+    def test_Version_from_string_validates(self):
+        self.assertRaises(ValueError, Version.from_string, 'a')
+        self.assertRaises(ValueError, Version.from_string, '1a:0')
+        self.assertRaises(ValueError, Version.from_string, '0:a.0.0-foo')
 
-    def test_split_full_version(self):
-        self.assertEqual(version.get_evr('00'), (0, '00', '0'))
-        self.assertEqual(version.get_evr('00-00'), (0, '00', '00'))
-        self.assertEqual(version.get_evr('0:0'), (0, '0', '0'))
-        self.assertEqual(version.get_evr('0:0-0'), (0, '0', '0'))
-        self.assertEqual(version.get_evr('0:0.0'), (0, '0.0', '0'))
-        self.assertEqual(version.get_evr('0:0.0-0'), (0, '0.0', '0'))
-        self.assertEqual(version.get_evr('0:0.0-00'), (0, '0.0', '00'))
+    def test_Version_from_string_tuples(self):
+        assert Version.from_string('00').tuple() == (0, '00', '0')
+        assert Version.from_string('00-00').tuple() == (0, '00', '00')
+        assert Version.from_string('0:0').tuple() == (0, '0', '0')
+        assert Version.from_string('0:0-0').tuple() == (0, '0', '0')
+        assert Version.from_string('0:0.0').tuple() == (0, '0.0', '0')
+        assert Version.from_string('0:0.0-0').tuple() == (0, '0.0', '0')
+        assert Version.from_string('0:0.0-00').tuple() == (0, '0.0', '00')
 
-    def test_get_alpha(self):
-        self.assertEqual(version.get_alphas(''), ('', ''))
-        self.assertEqual(version.get_alphas('0'), ('', '0'))
-        self.assertEqual(version.get_alphas('00'), ('', '00'))
-        self.assertEqual(version.get_alphas('0a'), ('', '0a'))
-        self.assertEqual(version.get_alphas('a'), ('a', ''))
-        self.assertEqual(version.get_alphas('a0'), ('a', '0'))
+    def test_get_non_digit_prefix(self):
+        assert get_non_digit_prefix('') == ''
+        assert get_non_digit_prefix('0') == ''
+        assert get_non_digit_prefix('00') == ''
+        assert get_non_digit_prefix('0a') == ''
+        assert get_non_digit_prefix('a') == 'a'
+        assert get_non_digit_prefix('a0') == 'a'
+        assert get_non_digit_prefix('aHAD0030') == 'aHAD'
 
-    def test_get_digits(self):
-        self.assertEqual(version.get_digits('00'), (0, ''))
-        self.assertEqual(version.get_digits('0'), (0, ''))
-        self.assertEqual(version.get_digits('0a'), (0, 'a'))
-        self.assertEqual(version.get_digits('a'), (0, 'a'))
-        self.assertEqual(version.get_digits('a0'), (0, 'a0'))
-
-    def test_listify(self):
-        self.assertEqual(version.listify('0'), ['', 0])
-        self.assertEqual(version.listify('00'), ['', 0])
-        self.assertEqual(version.listify('0a'), ['', 0, 'a', 0])
-        self.assertEqual(version.listify('a0'), ['a', 0])
-        self.assertEqual(version.listify('a00'), ['a', 0])
-        self.assertEqual(version.listify('a'), ['a', 0])
+    def test_get_digit_prefix(self):
+        assert get_digit_prefix('00') == 0
+        assert get_digit_prefix('0') == 0
+        assert get_digit_prefix('0a') == 0
+        assert get_digit_prefix('12a') == 12
+        assert get_digit_prefix('a') == 0
+        assert get_digit_prefix('a0') == 0
+        assert get_digit_prefix('arttr23123') == 0
+        assert get_digit_prefix('123sdf') == 123
+        assert get_digit_prefix('0123sdf') == 123
 
     def test_compare_strings(self):
-        self.assertEqual(version.compare_strings('~', '.'), -1)
-        self.assertEqual(version.compare_strings('~', 'a'), -1)
-        self.assertEqual(version.compare_strings('a', '.'), -1)
-        self.assertEqual(version.compare_strings('a', '~'), 1)
-        self.assertEqual(version.compare_strings('.', '~'), 1)
-        self.assertEqual(version.compare_strings('.', 'a'), 1)
-        self.assertEqual(version.compare_strings('.', '.'), 0)
-        self.assertEqual(version.compare_strings('0', '0'), 0)
-        self.assertEqual(version.compare_strings('a', 'a'), 0)
+        assert compare_strings('~', '.') == -1
+        assert compare_strings('~', 'a') == -1
+        assert compare_strings('a', '.') == -1
+        assert compare_strings('a', '~') == 1
+        assert compare_strings('.', '~') == 1
+        assert compare_strings('.', 'a') == 1
+        assert compare_strings('.', '.') == 0
+        assert compare_strings('0', '0') == 0
+        assert compare_strings('a', 'a') == 0
 
+    def test_compare_strings_can_sort(self):
         # taken from
         # http://www.debian.org/doc/debian-policy/ch-controlfields.html#s-f-Version
         result = sorted(['a', '', '~', '~~a', '~~'], key=version.compare_strings_key)
         expected = ['~~', '~~a', '~', '', 'a']
-        self.assertEqual(expected, result)
+        assert expected == result
 
-    def test_compare_revisions(self):
+    def test_compare_strings_more(self):
         # note that these are testing a single revision string, not the full
         # upstream+debian version.  IOW, "0.0.9-foo" is an upstream or debian
         # revision onto itself, not an upstream of 0.0.9 and a debian of foo.
 
         # equals
-        self.assertEqual(version.compare_revisions('0', '0'), 0)
-        self.assertEqual(version.compare_revisions('0', '00'), 0)
-        self.assertEqual(version.compare_revisions('00.0.9', '0.0.9'), 0)
-        self.assertEqual(version.compare_revisions('0.00.9-foo', '0.0.9-foo'), 0)
-        self.assertEqual(version.compare_revisions('0.0.9-1.00foo', '0.0.9-1.0foo'), 0)
+        assert compare_strings('0', '0') == 0
+        assert compare_strings('0', '00') == 0
+        assert compare_strings('00.0.9', '0.0.9') == 0
+        assert compare_strings('0.00.9-foo', '0.0.9-foo') == 0
+        assert compare_strings('0.0.9-1.00foo', '0.0.9-1.0foo') == 0
 
         # less than
-        self.assertEqual(version.compare_revisions('0.0.9', '0.0.10'), -1)
-        self.assertEqual(version.compare_revisions('0.0.9-foo', '0.0.10-foo'), -1)
-        self.assertEqual(version.compare_revisions('0.0.9-foo', '0.0.10-goo'), -1)
-        self.assertEqual(version.compare_revisions('0.0.9-foo', '0.0.9-goo'), -1)
-        self.assertEqual(version.compare_revisions('0.0.10-foo', '0.0.10-goo'), -1)
-        self.assertEqual(version.compare_revisions('0.0.9-1.0foo', '0.0.9-1.1foo'), -1)
+        assert compare_strings('0.0.9', '0.0.10') == -1
+        assert compare_strings('0.0.9-foo', '0.0.10-foo') == -1
+        assert compare_strings('0.0.9-foo', '0.0.10-goo') == -1
+        assert compare_strings('0.0.9-foo', '0.0.9-goo') == -1
+        assert compare_strings('0.0.10-foo', '0.0.10-goo') == -1
+        assert compare_strings('0.0.9-1.0foo', '0.0.9-1.1foo') == -1
 
         # greater than
-        self.assertEqual(version.compare_revisions('0.0.10', '0.0.9'), 1)
-        self.assertEqual(version.compare_revisions('0.0.10-foo', '0.0.9-foo'), 1)
-        self.assertEqual(version.compare_revisions('0.0.10-foo', '0.0.9-goo'), 1)
-        self.assertEqual(version.compare_revisions('0.0.9-1.0foo', '0.0.9-1.0bar'), 1)
+        assert compare_strings('0.0.10', '0.0.9') == 1
+        assert compare_strings('0.0.10-foo', '0.0.9-foo') == 1
+        assert compare_strings('0.0.10-foo', '0.0.9-goo') == 1
+        assert compare_strings('0.0.9-1.0foo', '0.0.9-1.0bar') == 1
 
     def test_compare_versions(self):
         # "This [the epoch] is a single (generally small) unsigned integer.
         # It may be omitted, in which case zero is assumed."
-        self.assertEqual(version.compare_versions('0.0.0', '0:0.0.0'), 0)
-        self.assertEqual(version.compare_versions('0:0.0.0-foo', '0.0.0-foo'), 0)
-        self.assertEqual(version.compare_versions('0.0.0-a', '0:0.0.0-a'), 0)
+        assert compare_versions('0.0.0', '0:0.0.0') == 0
+        assert compare_versions('0:0.0.0-foo', '0.0.0-foo') == 0
+        assert compare_versions('0.0.0-a', '0:0.0.0-a') == 0
 
-        # "The absence of a debian_revision is equivalent to a debian_revision
-        # of 0."
-        self.assertEqual(version.compare_versions('0.0.0', '0.0.0-0'), 0)
+        # "The absence of a debian_revision is equivalent to a debian_revision of 0."
+        assert compare_versions('0.0.0', '0.0.0-0') == 0
         # tricksy:
-        self.assertEqual(version.compare_versions('0.0.0', '0.0.0-00'), 0)
+        assert compare_versions('0.0.0', '0.0.0-00') == 0
 
         # combining the above
-        self.assertEqual(version.compare_versions('0.0.0-0', '0:0.0.0'), 0)
+        assert compare_versions('0.0.0-0', '0:0.0.0') == 0
 
         # explicitly equal
-        self.assertEqual(version.compare_versions('0.0.0', '0.0.0'), 0)
-        self.assertEqual(version.compare_versions('1:0.0.0', '1:0.0.0'), 0)
-        self.assertEqual(version.compare_versions('0.0.0-10', '0.0.0-10'), 0)
-        self.assertEqual(version.compare_versions('2:0.0.0-1', '2:0.0.0-1'), 0)
-        self.assertEqual(version.compare_versions('0:a.0.0-foo', '0:a.0.0-foo'), 0)
+        assert compare_versions('0.0.0', '0.0.0') == 0
+        assert compare_versions('1:0.0.0', '1:0.0.0') == 0
+        assert compare_versions('0.0.0-10', '0.0.0-10') == 0
+        assert compare_versions('2:0.0.0-1', '2:0.0.0-1') == 0
 
         # less than
-        self.assertEqual(version.compare_versions('0.0.0-0', '0:0.0.1'), -1)
-        self.assertEqual(version.compare_versions('0.0.0-0', '0:0.0.0-a'), -1)
-        self.assertEqual(version.compare_versions('0.0.0-0', '0:0.0.0-1'), -1)
-        self.assertEqual(version.compare_versions('0.0.9', '0.0.10'), -1)
-        self.assertEqual(version.compare_versions('0.9.0', '0.10.0'), -1)
-        self.assertEqual(version.compare_versions('9.0.0', '10.0.0'), -1)
+        assert compare_versions('0.0.0-0', '0:0.0.1') == -1
+        assert compare_versions('0.0.0-0', '0:0.0.0-a') == -1
+        assert compare_versions('0.0.0-0', '0:0.0.0-1') == -1
+        assert compare_versions('0.0.9', '0.0.10') == -1
+        assert compare_versions('0.9.0', '0.10.0') == -1
+        assert compare_versions('9.0.0', '10.0.0') == -1
+
+        assert compare_versions("1.2.3-1~deb7u1", "1.2.3-1") == -1
+        assert compare_versions("2.7.4+reloaded2-13ubuntu1", "2.7.4+reloaded2-13+deb9u1") == -1
+        assert compare_versions("2.7.4+reloaded2-13", "2.7.4+reloaded2-13+deb9u1") == -1
 
         # greater than
-        self.assertEqual(version.compare_versions('0.0.1-0', '0:0.0.0'), 1)
-        self.assertEqual(version.compare_versions('0.0.0-a', '0:0.0.0-1'), 1)
-        self.assertEqual(version.compare_versions('0.0.0-a', '0:0.0.0-0'), 1)
-        self.assertEqual(version.compare_versions('0.0.9', '0.0.1'), 1)
-        self.assertEqual(version.compare_versions('0.9.0', '0.1.0'), 1)
-        self.assertEqual(version.compare_versions('9.0.0', '1.0.0'), 1)
+        assert compare_versions('0.0.1-0', '0:0.0.0') == 1
+        assert compare_versions('0.0.0-a', '0:0.0.0-1') == 1
+        assert compare_versions('0.0.0-a', '0:0.0.0-0') == 1
+        assert compare_versions('0.0.9', '0.0.1') == 1
+        assert compare_versions('0.9.0', '0.1.0') == 1
+        assert compare_versions('9.0.0', '1.0.0') == 1
+
+        assert compare_versions("1.2.3-1", "1.2.3-1~deb7u1") == 1
+        assert compare_versions("2.7.4+reloaded2-13+deb9u1", "2.7.4+reloaded2-13ubuntu1") == 1
+        assert compare_versions("2.7.4+reloaded2-13+deb9u1", "2.7.4+reloaded2-13") == 1
 
         # unicode
-        self.assertEqual(version.compare_versions(u'2:0.0.44-1', u'2:0.0.44-nobin'), -1)
-        self.assertEqual(version.compare_versions(u'2:0.0.44-nobin', u'2:0.0.44-1'), 1)
-        self.assertEqual(version.compare_versions(u'2:0.0.44-1', u'2:0.0.44-1'), 0)
+        assert compare_versions(u'2:0.0.44-1', u'2:0.0.44-nobin') == -1
+        assert compare_versions(u'2:0.0.44-nobin', u'2:0.0.44-1') == 1
+        assert compare_versions(u'2:0.0.44-1', u'2:0.0.44-1') == 0
---
+++ tests/test_version_python_deb_pkg_tools.py
@@ -0,0 +1,50 @@
+
+# Debian packaging tools: Automated tests.
+#
+# Author: Peter Odding <peter@peterodding.com>
+# Last Change: March 9, 2021
+# URL: https://github.com/xolox/python-deb-pkg-tools
+
+# SPDX-License-Identifier: MIT
+
+from unittest import TestCase
+
+from debut.version import Version
+
+
+class DebPkgToolsTestCase(TestCase):
+
+    def test_version_sorting(self):
+        # Check version sorting implemented on top of `=' and `<<' comparisons.
+        expected_order = ['0.1', '0.5', '1.0', '2.0', '3.0', '1:0.4', '2:0.3']
+        assert list(sorted(expected_order)) != expected_order
+        result = [str(v) for v in sorted(map(Version.from_string, expected_order))]
+        assert result == expected_order
+
+    def test_version_comparison(self):
+        assert Version.from_string('1.0') > Version.from_string('0.5')
+        assert Version.from_string('1:0.5') > Version.from_string('2.0')
+        assert not Version.from_string('0.5') > Version.from_string('2.0')
+
+        assert Version.from_string('0.75') >= Version.from_string('0.5')
+        assert Version.from_string('0.50') >= Version.from_string('0.5')
+        assert Version.from_string('1:0.5') >= Version.from_string('5.0')
+        assert not Version.from_string('0.2') >= Version.from_string('0.5')
+
+        assert Version.from_string('0.5') < Version.from_string('1.0')
+        assert Version.from_string('2.0') < Version.from_string('1:0.5')
+        assert not Version.from_string('2.0') < Version.from_string('0.5')
+
+        assert Version.from_string('0.5') <= Version.from_string('0.75')
+        assert Version.from_string('0.5') <= Version.from_string('0.50')
+        assert Version.from_string('5.0') <= Version.from_string('1:0.5')
+        assert not Version.from_string('0.5') <= Version.from_string('0.2')
+
+        assert Version.from_string('42') == Version.from_string('42')
+        assert Version.from_string('0.5') == Version.from_string('0:0.5')
+        assert not Version.from_string('0.5') == Version.from_string('1.0')
+
+        assert Version.from_string('1') != Version.from_string('0')
+        assert not Version.from_string('0.5') != Version.from_string('0:0.5')
+
+        assert Version.from_string("1.3~rc2") < Version.from_string("1.3")
