b53a888fd13f6b9bf43bfe68a34d21847d9cc3fe
Philippe Ombredanne <pombredanne@nexb.com>
2021-10-30 10:21:25 +0200

    Format code
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/test_version_specifier.py
+++ tests/test_version_specifier.py
@@ -13,14 +13,15 @@ from univers.version_specifier import VersionSpecifier
 class TestVersionSpecifier(TestCase):
     def test_from_version_spec_string(self):
         spec_string = "pypi:0.0.2,0.0.6,0.0.0,0.0.1,0.0.4,0.0.5,0.0.3"
-        version_spec = VersionSpecifier.from_version_spec_string(spec_string)
-
+        version_spec = VersionSpecifier.from_version_spec_string(version_spec_string=spec_string)
+        # TODO: test all values
         assert len(version_spec.ranges) == 7
         assert all(["pypi" == vrange.version.scheme for vrange in version_spec.ranges])
 
     def test_resolving_pessimsitic_operator(self):
         version_range_string = "~>2.0.8"
         version_spec = VersionSpecifier.from_scheme_version_spec_string(
-            "semver", version_range_string
+            scheme="semver", 
+            value=version_range_string,
         )
         assert len(version_spec.ranges) == 2
