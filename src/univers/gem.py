6a73d2a2edc32978c18f255fa0d7bd9e3ec2f358
Philippe Ombredanne <pombredanne@nexb.com>
2021-11-23 14:52:36 +0100

    Move code for univers integration
    Move methid from init in module
    Add original license notices to code
    Rename paths to match univers
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/univers/gem.py
+++ src/univers/gem.py
@@ -1,9 +1,33 @@
+# Copyright 2017 Center for Information Technology
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import re
 
 from six import iterkeys, itervalues
+from typing import Type, TypeVar
 from typing import Callable, Sequence, MutableSequence
 
-from puppeter.domain import default
+
+T = TypeVar('T')
+
+
+def default(x, e, y):
+    # type: (Callable[[], T], Type[Exception], T) -> T
+    try:
+        return x()
+    except e:
+        return y
 
 
 class GemVersion:
