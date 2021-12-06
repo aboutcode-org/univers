09affebd49aa0d16a628f5091f6fe25932ce2227
Philippe Ombredanne <pombredanne@nexb.com>
2021-12-06 11:56:43 +0100

    Port to Python
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/test_bundler_version_ranges_spec.py
+++ tests/test_bundler_version_ranges_spec.py
@@ -1,4 +1,3 @@
-
 #
 # Copyright (c) Chad Fowler, Rich Kilmer, Jim Weirich and others.
 # Portions copyright (c) Engine Yard and Andre ArkoFacebook, Inc. and its affiliates.
@@ -8,41 +7,45 @@
 # Originally from https://github.com/rubygems/rubygems
 
 
-require "bundler/version_ranges"
+from univers.gem import GemRequirement
 
-RSpec.describe Bundler::VersionRanges do
-  describe ".empty?" do
-    shared_examples_for "empty?" do |exp, *req|
-      it "returns #{exp} for #{req}" do
-        r = Gem::Requirement.new(*req)
-        ranges = described_class.for(r)
-        expect(described_class.empty?(*ranges)).to eq(exp), "expected `#{r}` #{exp ? "" : "not "}to be empty"
-      end
-    end
 
-    include_examples "empty?", false
-    include_examples "empty?", false, "!= 1"
-    include_examples "empty?", false, "!= 1", "= 2"
-    include_examples "empty?", false, "!= 1", "> 1"
-    include_examples "empty?", false, "!= 1", ">= 1"
-    include_examples "empty?", false, "= 1", ">= 0.1", "<= 1.1"
-    include_examples "empty?", false, "= 1", ">= 1", "<= 1"
-    include_examples "empty?", false, "= 1", "~> 1"
-    include_examples "empty?", false, ">= 0.z", "= 0"
-    include_examples "empty?", false, ">= 0"
-    include_examples "empty?", false, ">= 1.0.0", "< 2.0.0"
-    include_examples "empty?", false, "~> 1"
-    include_examples "empty?", false, "~> 2.0", "~> 2.1"
-    include_examples "empty?", true, ">= 4.1.0", "< 5.0", "= 5.2.1"
-    include_examples "empty?", true, "< 5.0", "< 5.3", "< 6.0", "< 6", "= 5.2.0", "> 2", ">= 3.0", ">= 3.1", ">= 3.2", ">= 4.0.0", ">= 4.1.0", ">= 4.2.0", ">= 4.2", ">= 4"
-    include_examples "empty?", true, "!= 1", "< 2", "> 2"
-    include_examples "empty?", true, "!= 1", "<= 1", ">= 1"
-    include_examples "empty?", true, "< 2", "> 2"
-    include_examples "empty?", true, "< 2", "> 2", "= 2"
-    include_examples "empty?", true, "= 1", "!= 1"
-    include_examples "empty?", true, "= 1", "= 2"
-    include_examples "empty?", true, "= 1", "~> 2"
-    include_examples "empty?", true, ">= 0", "<= 0.a"
-    include_examples "empty?", true, "~> 2.0", "~> 3"
-  end
-end
+def test_is_empty():
+    assert not GemRequirement("!= 1").is_empty()
+    assert not GemRequirement("!= 1", "= 2").is_empty()
+    assert not GemRequirement("!= 1", "> 1").is_empty()
+    assert not GemRequirement("!= 1", ">= 1").is_empty()
+    assert not GemRequirement("= 1", ">= 0.1", "<= 1.1").is_empty()
+    assert not GemRequirement("= 1", ">= 1", "<= 1").is_empty()
+    assert not GemRequirement("= 1", "~> 1").is_empty()
+    assert not GemRequirement(">= 0.z", "= 0").is_empty()
+    assert not GemRequirement(">= 0").is_empty()
+    assert not GemRequirement(">= 1.0.0", "< 2.0.0").is_empty()
+    assert not GemRequirement("~> 1").is_empty()
+    assert not GemRequirement("~> 2.0", "~> 2.1").is_empty()
+    assert GemRequirement(">= 4.1.0", "< 5.0", "= 5.2.1").is_empty()
+    assert GemRequirement(
+        "< 5.0",
+        "< 5.3",
+        "< 6.0",
+        "< 6",
+        "= 5.2.0",
+        "> 2",
+        ">= 3.0",
+        ">= 3.1",
+        ">= 3.2",
+        ">= 4.0.0",
+        ">= 4.1.0",
+        ">= 4.2.0",
+        ">= 4.2",
+        ">= 4",
+    ).is_empty()
+    assert GemRequirement("!= 1", "< 2", "> 2").is_empty()
+    assert GemRequirement("!= 1", "<= 1", ">= 1").is_empty()
+    assert GemRequirement("< 2", "> 2").is_empty()
+    assert GemRequirement("< 2", "> 2", "= 2").is_empty()
+    assert GemRequirement("= 1", "!= 1").is_empty()
+    assert GemRequirement("= 1", "= 2").is_empty()
+    assert GemRequirement("= 1", "~> 2").is_empty()
+    assert GemRequirement(">= 0", "<= 0.a").is_empty()
+    assert GemRequirement("~> 2.0", "~> 3").is_empty()
