c6656ae2ec1437e9d47c59fa9b3a7e50ba8d3279
Philippe Ombredanne <pombredanne@nexb.com>
2021-12-06 09:44:26 +0100

    Add ABOUT files, document origin and license
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/univers/rpm.py
+++ src/univers/rpm.py
@@ -1,23 +1,9 @@
+#
 # Copyright (c) Facebook, Inc. and its affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-#
 # Copyright (c) SAS Institute Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-License-Identifier: MIT AND Apache-2.0
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import re
 from typing import NamedTuple
 from typing import Union
@@ -59,30 +45,21 @@ def from_evr(s):
     return e, v, r
 
 
-# This comprises a pure python implementation of rpm version comparison. The
-# purpose for this is so that the antlir library does not have a dependency
-# on a C library that is (for the most part) only distributed as part of rpm
-# based distros. Depending on a C library complicates dependency management
-# significantly in the OSS space due to the complexity of handling 3rd party C
-# libraries with buck. Having this pure python implementation also eases future
-# rpm usage/handling for both non-rpm based distros and different arch types.
-#
-# This implementation is adapted from both this blog post:
-#  https://blog.jasonantman.com/2014/07/how-yum-and-rpm-compare-versions/
-# and this Apache 2 licensed implementation:
-#   https://github.com/sassoftware/python-rpm-vercmp/blob/master/rpm_vercmp/vercmp.py
-#
-# There are extensive test cases in the `test_rpm_metadata.py` test case that
-# cover a wide variety of normal and weird version comparsions.
-
-
 def compare_rpm_versions(a: Union[RpmVersion, str], b: Union[RpmVersion, str]) -> int:
     """
-    Returns:
-        1 if the version of a is newer than b
-        0 if the versions match
-        -1 if the version of a is older than b
+    Compare to RPM versions ``a`` and ``b`` and return:
+    -  1 if the version of a is newer than b
+    -  0 if the versions match
+    -  -1 if the version of a is older than b
 
+    These are the legacy "cmp()" function semantics.
+
+    This implementation is adapted from both this blog post:
+    https://blog.jasonantman.com/2014/07/how-yum-and-rpm-compare-versions/
+    and this Apache 2 licensed implementation:
+    https://github.com/sassoftware/python-rpm-vercmp/blob/master/rpm_vercmp/vercmp.py
+
+    For example::
     >>> assert compare_rpm_versions("1.0", "1.1") == -1
     >>> assert compare_rpm_versions("1.1", "1.0") == 1
     >>> assert compare_rpm_versions("11.13.2-1", "11.13.2.0-1") == -1
