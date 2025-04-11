7af789f39b473212415541ecd8bdf739ec409ade
Kunz, Immanuel <immanuel.kunz@aisec.fraunhofer.de>
2025-04-11 17:03:50 +0200

    use byte comparison

--- src/univers/versions.py
+++ src/univers/versions.py
@@ -142,8 +142,18 @@ class LexicographicVersion(Version):
     Create a string, even if, e.g., an integer is given
     """
 
+    @classmethod
     def normalize(cls, string):
         return remove_spaces(str(string))
+
+    def __lt__(self, other):
+        return self.value.encode('utf-8') < other.value.encode('utf-8')
+
+    def __gt__(self, other):
+        return self.value.encode('utf-8') > other.value.encode('utf-8')
+
+    def __eq__(self, other):
+        return self.value.encode('utf-8') == other.value.encode('utf-8')
 
 
 class GenericVersion(Version):
